cef731b3a14ebe0fee481e8824d001443ce743ba
frillyguidance23 <cbereznitsky1@gmail.com>
2025-07-21 14:01:27 -0500

    Added background and exit functionality

--- main.py
+++ main.py
@@ -1,10 +1,17 @@
-import constants
+from constants import *
 import pygame
 
 def main():
-    print("Starting Asteroids!")
-    print(f"Screen width: {constants.SCREEN_WIDTH}")
-    print(f"Screen height: {constants.SCREEN_HEIGHT}")
+    pygame.init
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    while True:
+        BLACK = (0,0,0)
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+
+        screen.fill(BLACK)
+        pygame.display.flip()
 
 
 if __name__ == "__main__":
